6a01db89a1032b9911764cd7a0ed7a795d3b554b
dancingshell <kaitlinjaffe@gmail.com>
2014-03-31 19:42:19 -0700

    adding comments

--- array.rb
+++ array.rb
@@ -1,9 +1,9 @@
-array=[]
-entry=gets.chomp
-array.push entry
+array=[] #empty array to be filled
+entry=gets.chomp #getting new entry
+array.push entry #adding entry to array
 while entry!=''
-  entry=gets.chomp
-  array.push entry
+  entry=gets.chomp #gets new entry if previous entry not blank
+  array.push entry #adding entry to array
 end
-array.pop
-puts array.sort.join(', ')
+array.pop #comps last blank entry of array
+puts array.sort.join(', ') #prints and sorts array
