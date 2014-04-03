b19f182d3f00a29284d7d418c4a0c6318f5526b7
dancingshell <kaitlinjaffe@gmail.com>
2014-04-02 17:24:20 -0700

    different layout

--- is_prime.rb
+++ is_prime.rb
@@ -1,14 +1,25 @@
 def is_prime(num)
-  denominator=2
   
-  while num>=denominator
-    if num%denominator!=0
+denominator=2
+test=num%denominator
+  
+while num>=denominator #&& (num!=1 || num!=2)
+  test
+    if test==0
+      puts ''+num.to_s+' is not prime (false)'
+      break
+    # if num==1 || num==2
+#       break
+    else
       denominator+=1
-    else break
-      puts ''+num+' is false'
     end
-    puts ''+num+'is true' 
+    puts ''+num.to_s+' is prime (true)'
   end
+
 end
 
-puts is_prime(6)
+is_prime(4)
+is_prime(27)
+is_prime(98)
+is_prime(2)
+is_prime(5)
